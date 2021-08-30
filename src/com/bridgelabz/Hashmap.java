a8ea04990de5196d6d0e724279c6e85123caa149
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-30 15:17:58 +0530

    Added:Welcome message

---
+++ src/com/bridgelabz/Hashmap.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+public class Hashmap {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hashmap");
+    }
+}
